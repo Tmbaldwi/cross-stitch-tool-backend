965669ff4bbefba5b2975dbd9aab633639b580f1
Tmbaldwi <Tyler.Baldwin.Work@gmail.com>
2024-08-26 21:44:02 -0700

    backend function test

--- api/views.py
+++ api/views.py
@@ -2,14 +2,25 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import TestTable
 from .serializers import TestTableSerializer
+from django.db import connection
 
 class TestTableView(APIView):
     def get(self, request):
         # Data Retrieval
-        first_entry = TestTable.objects.first()  # Retrieves the first record from test_table
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM test_function()")
+            results = cursor.fetchall()
 
-        # Serialization
-        serializer = TestTableSerializer(first_entry)
+        if results:
+            data = [
+                {"id": row[0], "testname": row[1], "addr": row[2]}
+                for row in results
+            ]
+
+            # Serialization
+            serializer = TestTableSerializer(data, many=True)
+
+            return Response(serializer.data)
 
         # Response
-        return Response(serializer.data)
+        return Response({"error": "No data found"}, status=404)
