bdc89594896d911c2dfb92d611f14bea678db9ca
Tmbaldwi <Tyler.Baldwin.Work@gmail.com>
2024-08-31 19:31:32 -0700

    refactoring and image saving endpoint (UNTESTED)

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import TestTable
+from .models.test_models import TestTable
 
 class TestTableSerializer(serializers.ModelSerializer):
     class Meta:
--- api/urls.py
+++
@@ -1,8 +0,0 @@
-# backend/api/urls.py
-
-from django.urls import path
-from .views import TestTableView
-
-urlpatterns = [
-    path('test/', TestTableView.as_view(), name='test-table'),
-]
