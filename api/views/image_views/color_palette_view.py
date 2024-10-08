7cafbbbe2002d6c94fa2a48604292a21f92419bb
Tmbaldwi <Tyler.Baldwin.Work@gmail.com>
2024-10-07 21:44:51 -0700

    updated existing workflows to use compressed image

--- api/views/image_views/color_palette_view.py
+++ api/views/image_views/color_palette_view.py
@@ -3,11 +3,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from api.scripts.image_processing import get_palette_and_closest_colors
 from api.models.palette_model import palettes_to_json
+from api.scripts.utility.image_processing_utility import compressed_original_image_path
 
 class ColorPaletteView(APIView):
     def get(self, request):
 
-        palette, color_palette = get_palette_and_closest_colors()
+        palette, color_palette = get_palette_and_closest_colors(compressed_original_image_path)
 
         if color_palette == None or palette == None:
             return Response({"message": "Color palette could not be created"}, status=status.HTTP_404_NOT_FOUND)
--- api/views/image_views/reset_image_view.py
+++ api/views/image_views/reset_image_view.py
@@ -4,12 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import FileResponse
-from api.scripts.utility.image_processing_utility import image_path_valid
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-mod_image_path = os.path.join(current_dir, '..', '..', '..', 'media', 'modified_image.png')
-orig_image_path = os.path.join(current_dir, '..', '..', '..', 'media', 'original_image.png')
+from api.scripts.utility.image_processing_utility import image_path_valid, mod_image_path, orig_image_path, compressed_original_image_path
 
 
 class ResetImageView(APIView):
@@ -18,9 +13,9 @@ class ResetImageView(APIView):
         if not image_path_valid(mod_image_path):
             return Response({"message": "Modified image could not be found"}, status=status.HTTP_404_NOT_FOUND)
         
-        if not image_path_valid(orig_image_path):
-            return Response({"message": "Original image could not be found"}, status=status.HTTP_404_NOT_FOUND)
+        if not image_path_valid(compressed_original_image_path):
+            return Response({"message": "Compressed original image could not be found"}, status=status.HTTP_404_NOT_FOUND)
 
-        shutil.copyfile(orig_image_path, mod_image_path)
+        shutil.copyfile(compressed_original_image_path, mod_image_path)
 
         return FileResponse(open(mod_image_path, 'rb'), content_type='image/jpeg')
